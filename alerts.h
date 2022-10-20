d99b8b359a09739c4cfaf266e79a68f876d92b8f
NabilCob <69570628+NabilCob@users.noreply.github.com>
2022-10-20 10:59:58 +0530

    Update alerts.h
    Added declarations

--- alerts.h
+++ alerts.h
@@ -1,9 +1,16 @@
 #ifndef ALERTS_H_
 #define ALERTS_H_
 
+//Type declartion
+typedef void (*alerter_funcptr)();
 
+//Variable declaration
+extern unsigned int emailAlertCallCount;
+extern unsigned int ledAlertCallCount;
 
-
-
+//Function declaration
+extern void check_and_alert(float maxThreshold, alerter_funcptr alerters[], int alerterslen, Stats_t computedStats);
+extern void emailAlerter(void);
+extern void ledAlerter(void);
 
 #endif
