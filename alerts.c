bc8ea7e3e3703f8a183971bc4abd96af52579035
NabilCob <69570628+NabilCob@users.noreply.github.com>
2022-10-20 11:10:29 +0530

    Update alerts.c
    - Update alerter functions

--- alerts.c
+++ alerts.c
@@ -1,5 +1,30 @@
+#include "stats.h"
+#include "alerts.h"
 
+//Variable definition for alerter call count
+unsigned int emailAlertCallCount = 0;
+unsigned int ledAlertCallCount = 0;
 
+void check_and_alert(const float maxThreshold, alerter_funcptr alert[], int alerterslen, Stats_t computedStats){
 
+    int idx;
+    if(computedStats.max > maxThreshold){
+       //Run a loop to call the alert function configured
+       for(idx = 0 ; idx < alerterslen; idx ++){
+           (*alert[idx])();
+       }
+    }
+}
 
+//email alerter functions
+void emailAlerter(void){
+    emailAlertCallCount ++;
+    //Usecase
+}
+
+//led aleter functions
+void ledAlerter(void){
+    ledAlertCallCount ++;
+    //Usecase
+}
 
